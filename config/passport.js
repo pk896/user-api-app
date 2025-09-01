d2d52a577a84eb19bc0b6004c4c6dbe2564aa41e
Phakisi Ngxongxela <phakisingxongxela@gmail.com>
2025-09-01 18:59:41 +0200

    update

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,61 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 const crypto = require('crypto');
 
+// Determine callback URL based on environment
+const GOOGLE_CALLBACK_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://my-express-server-rq4a.onrender.com/auth/google/callback'
+    : 'http://localhost:3000/auth/google/callback';
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ email: profile.emails[0].value });
+
+        if (!user) {
+          user = await User.create({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            password: crypto.randomBytes(16).toString('hex'), // dummy password
+          });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+module.exports = passport;
+
+
+
+
+// config/passport.js
+/*const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+const crypto = require('crypto');
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -37,4 +92,4 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-module.exports = passport;
+module.exports = passport;*/
